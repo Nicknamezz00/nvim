5d62f31ab21d7f7abc1979feb3b78606d78ec904
wrz <1315098969@qq.com>
2024-07-01 21:28:02 +0800

    up

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  styles = {
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = { italic = false },
+    variables = { italic = false },
+    statements = { italic = false },
+    conditionals = { italic = false },
+    loops = { italic = false },
+    constants = { italic = false },
+    numbers = { italic = false },
+    operators = { italic = false },
+    types = { italic = false },
+  },
+  opts = {
+    style = "night",
+  },
+}
